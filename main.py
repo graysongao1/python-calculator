8b76a950377e9ff6b47795b66587b412d849d475
graysongao1 <gaograyson661@gmail.com>
2024-09-12 17:00:57 -0400

    this is my second commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.configure(height=500, width=500)
+root.wm_resizable(False, False)
+root.title("feet to meters")
+
+style = ttk.Style()
+style.configure("custom.TFrame", background="#eb4034")
+
+mainframe = ttk.Frame(root, style="custom.TFrame", padding="100 100 100 100")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.mainloop()
